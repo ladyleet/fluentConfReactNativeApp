9ba11d613b5e6882e65b07ad8201c841b05c5da0
Tracy Lee <tracy@thisdot.co>
2017-06-18 16:47:35 -0700

    add components and images for jeopardy

---
+++ src/Vue.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  margin: 20px;
+`;
+
+export default class VueJS extends Component {
+  render() {
+    return (
+      <Container>
+        <Image source={require('./img/vue.png')}/>
+      </Container>
+    );
+  }
+}
